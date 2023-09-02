d9e64cf2a9162b39c2ff0fac674b6909040616bf
Vilas-1234 <140586901+Vilas-1234@users.noreply.github.com>
2023-09-02 14:42:01 +0530

    Update OnlineBookMyShowApplication.java
    here u write unwanted code

--- src/main/java/in/vp/main/OnlineBookMyShowApplication.java
+++ src/main/java/in/vp/main/OnlineBookMyShowApplication.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-fdsjfdsjkf
+
 @ComponentScan(basePackages = {"in.vp.repository"})
 public class OnlineBookMyShowApplication extends SpringBootServletInitializer{
 
